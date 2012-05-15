b748cf470c2a0dd6fea7c3f1ab75f9bfbb61a3e5
Benson <benson@loaner1.dhcp.lbl.gov>
2012-05-15 16:07:59 -0700

    running with shifted S288C

--- testn.py
+++ testn.py
@@ -11,7 +11,7 @@ BLASTN_RESULTS_DIR = 'resultsn/'
 #'T73_AFDF01000000', 'UC5_AFDD01000000', 'Vin13_ADXC01000000', 'VL3_AEJS01000000', 'Y10_AEWK01000000', \
 #'YJM269_AEWN01000000', 'YJM789_AAFW02000000', 'YPS163_ABPD01000000', 'W303_MPG_2011']
 
-MAIN_SPECIES='S288C_cerevisiae'
+MAIN_SPECIES='S288C_cerevisiae__shift1'
 #SUBJECT_DBS = ['Ashbya_gossypii', 'Candida_glabrata', 'K_waltii', 'Kluyveromyces_lactis', \
 #'Kluyveromyces_thermotolerans', 'S_castellii_040406', 'Saccharomyces_kluyveri']
 #GG=['S_bayanus_ORFs.fasta', 'S_mikatae_ORFs.fasta', 'S_paradoxus_ORFs.fasta']
@@ -27,7 +27,7 @@ SUBJECT_DBS = ['Ashbya_gossypii', 'Candida_glabrata', 'K_waltii', 'Kluyveromyces
 'lodderomyces_elongisporus_1', 'magnaporthe_grisea_70-15_mitochondria_6', 'magnaporthe_grisea__m._oryzae__70-15_6', \
 'neosartorya_fischeri', 'neurospora_crassa_or74a__finished__10', 'schizosaccharomyces_cryophilus_oy26_3', \
 'schizosaccharomyces_japonicus_yfs275_4', 'schizosaccharomyces_octosporus_5', 'schizosaccharomyces_pombe_972h-_2', \
-'sclerotinia_sclerotiorum_2', 'uncinocarpus_reesii_2', 'S_bayanus', 'S_mikatae', 'S_paradoxus']
+'sclerotinia_sclerotiorum_2', 'uncinocarpus_reesii_2']#, 'S_bayanus', 'S_mikatae', 'S_paradoxus']
 
 
 # RUN
